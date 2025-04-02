da9b48c37c93f073c64d4b1f20b2bc7475c38f1f
Mohammad Talim <talimmohammad116@gmail.com>
2025-04-02 08:33:40 +0000

    respond to an HTTP request with a 200 response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,8 +9,17 @@ public class Main {
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
-            serverSocket.accept();
+            Socket clientSocket = serverSocket.accept();
             System.out.println("accepted new connection");
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            // Send HTTP 200 OK response
+            out.write("HTTP/1.1 200 OK\r\n");
+            out.write("\r\n");
+            out.flush();
+
+            out.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
