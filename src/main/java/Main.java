c6965ed21a5cdd20f691147369f29f1be2f8117b
Mohammad Talim <talimmohammad116@gmail.com>
2025-04-02 08:57:40 +0000

    extract the URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,13 +14,29 @@ public class Main {
             Socket clientSocket = serverSocket.accept();
             System.out.println("accepted new connection");
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-            // Send HTTP 200 OK response
-            out.write("HTTP/1.1 200 OK\r\n");
-            out.write("\r\n");
-            out.flush();
+            String requestLine = in.readLine();
+            if (requestLine != null) {
+                String[] requestParts = requestLine.split(" ");
+                if (requestParts.length < 2) {
+                    return;
+                }
 
+                String _ = requestParts[0];
+                String path = requestParts[1];
+
+                if (path.equals("/")) {
+                    out.write("HTTP/1.1 200 OK\r\n");
+                    out.write("\r\n");
+                } else {
+                    out.write("HTTP/1.1 404 Not Found\r\n");
+                    out.write("\r\n");
+                }
+            }
+
+            out.flush();
             out.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
